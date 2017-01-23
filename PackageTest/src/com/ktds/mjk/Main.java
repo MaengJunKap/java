dc0fff5068e9fe3816305507d3ed599a4dd928ba
206-005\206-005 <anxious1235@gmail.com>
2017-01-23 11:29:23 +0900

    2017년 1월23일 패키지 예제

---
+++ PackageTest/src/com/ktds/mjk/Main.java
@@ -0,0 +1,14 @@
+package com.ktds.mjk;
+
+import com.ktds.mjk.hospital.Doctor;
+
+public class Main {
+	
+	public static void main(String[] args){
+		
+		System.out.println("테스트");
+		
+		Doctor doctor = new Doctor();
+		doctor.hello();
+	}
+}
---
+++ PackageTest/src/com/ktds/mjk/hospital/Doctor.java
@@ -0,0 +1,13 @@
+package com.ktds.mjk.hospital;
+
+public class Doctor {
+
+		public void hello (){
+			
+			System.out.println("a");
+			
+		} 
+	
+	}
+
+
