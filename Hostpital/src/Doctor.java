60fbf42e1608f833e7789d50f1fbea31569184a3
owner <owner@DB400S2A-PC101>
2017-02-07 20:09:03 +0900

    클래스 수정

--- Hostpital/src/Doctor.java
+++ Hostpital/src/Doctor.java
@@ -1,16 +1,36 @@
 
+
 public class Doctor {
 
-	private String computer = "전산시스템";
-	private String medicalAppliance = "청진기";
+	private String computer ;
+	private String medicalAppliance ;  //인스턴스변수는 생성과 동시 값을 가지면 안된다. 생성자를 생성하는게 효율적 
+	
+	public Doctor(){
+		this.computer = "전산시스템";
+		this.medicalAppliance = "청진기"; 
+	}
+	public Doctor(String name, String computer, String medicalappliance){ //값을 초기화할때 생성자 
+		System.out.println("Docter 생성자가 호출되었습니다.");
+		this.computer = computer;
+		this.medicalAppliance  = medicalappliance;
+		System.out.println(name+"객체가 생성되었습니다.");
+	}
 
 	// 생성자
 	public void setComputer(String computer) {
 		this.computer = computer;
 	}
 
+	public String getComputer() {
+		return computer;
+	}
+
 	public void setMedicalAppliance(String medicalAppliance) {
 		this.medicalAppliance = medicalAppliance;
+	}
+
+	public String getMedicalAppliance() {
+		return medicalAppliance;
 	}
 
 	// 환자 병적 조회
@@ -25,7 +45,7 @@ public class Doctor {
 
 	// 처방전 작성
 	public void prescription() {
-		System.out.println("의사가" + medicalAppliance + "로 환자의 처방전을 작성합니다.");
+		System.out.println("의사가" + computer + "로 환자의 처방전을 작성합니다.");
 	}
 
 }
--- Hostpital/src/Hostpital.java
+++ Hostpital/src/Hostpital.java
@@ -2,15 +2,21 @@
 public class Hostpital {
 
 	public static void main(String[] args) {
-
-		Doctor 김사부 = new Doctor();
-		김사부.setComputer("MRI");
+		//가로있는건 메소드 생성자 가로안에서 값을 변경가능
+		Doctor 김사부 = new Doctor("김사부","아이패드","체온계"); //생성자에 있는 타입에 맞게 값을 넣는다. 
+	   //김사부.setComputer("MRI");
 		김사부.inquery();
 		김사부.treatment();
+		
+		String computer = 김사부.getComputer();
+		String meString = 김사부.getMedicalAppliance();
+		System.out.println("김사부의 컴퓨터는" + computer + "입니다.");
+		System.out.println("김사부의 진료도구는 " + meString + "입니다.");
 
 		Doctor 봉달희 = new Doctor();
 		봉달희.treatment();
 		봉달희.prescription();
+
 	}
 
 }
